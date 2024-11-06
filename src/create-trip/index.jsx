b1f1ef6e19b3106f97fab731307d6ca64f289acc
jagdishbutte <jagdishbutte@gmail.com>
2024-11-06 17:23:13 +0530

    Google Authentication Successful

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,50 +1,99 @@
 import { Input } from "@/components/ui/input";
 import React, { useEffect, useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
-import { AI_PROMPT, SelectBudgetOptions, SelectTravelesList } from "../constants/options";
+import {
+  AI_PROMPT,
+  SelectBudgetOptions,
+  SelectTravelesList,
+} from "../constants/options";
 import { Button } from "../components/ui/button";
 import { toast } from "sonner";
 import { chatSession } from "../service/AIModel";
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { FcGoogle } from "react-icons/fc";
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 function CreateTrip() {
   const [place, setPlace] = useState();
 
   const [formData, setFormData] = useState([]);
 
-  const handleInputChange=(name, value)=>{
+  const [openDialog, setOpenDialog] = useState(false);
 
+  const handleInputChange = (name, value) => {
     setFormData({
       ...formData,
-      [name]:value
-    })
-  }
+      [name]: value,
+    });
+  };
 
-  useEffect(()=>{
-    console.log(formData)
-  },[formData])
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
 
-  const onGenerateTrip=async()=>{
-    if(formData?.noOfDays>5 && !formData?.location || !formData?.budget || !formData?.traveler){
-      toast("Please fill all details...")
-      return ;
+  const login = useGoogleLogin({
+    onSuccess:(codeResp) => getUserProfile(codeResp),
+    onError:(error) => console.log(error),
+  });
+
+  const onGenerateTrip = async () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (!user) {
+      setOpenDialog(true);
+      return;
     }
-    const FINAL_PROMPT = AI_PROMPT
-    .replace('{location}',formData?.location.label)
-    .replace('{totalDays}',formData?.noOfDays)
-    .replace('{traveler}',formData?.traveler)
-    .replace('{budget}',formData?.budget)
-    .replace('{totalDays}',formData?.noOfDays)
-    
-    console.log(FINAL_PROMPT);
-    const result = await chatSession.sendMessage(FINAL_PROMPT)
 
-    console.log(result?.response?.text())
+    if (
+      (formData?.noOfDays > 5 && !formData?.location) ||
+      !formData?.budget ||
+      !formData?.traveler
+    ) {
+      toast("Please fill all details...");
+      return;
+    }
+    const FINAL_PROMPT = AI_PROMPT.replace(
+      "{location}",
+      formData?.location.label
+    )
+      .replace("{totalDays}", formData?.noOfDays)
+      .replace("{traveler}", formData?.traveler)
+      .replace("{budget}", formData?.budget)
+      .replace("{totalDays}", formData?.noOfDays);
+
+    console.log(FINAL_PROMPT);
+    const result = await chatSession.sendMessage(FINAL_PROMPT);
+
+    console.log(result?.response?.text());
+  };
+
+  const getUserProfile=(tokenInfo)=>{
+    axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenInfo?.access_token}`, {
+      headers:{
+        Authorization: `Bearer ${tokenInfo?.access_token}`,
+        Accept: 'Application/json'
+      }
+    }).then((resp)=>{
+      console.log(resp);
+      localStorage.setItem('user', JSON.stringify(resp.data));
+      setOpenDialog(false);
+      onGenerateTrip();
+    })
   }
 
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
-      <h2 className="font-bold text-3xl">Tell us your travel preferences🏕️🌴</h2>
+      <h2 className="font-bold text-3xl">
+        Tell us your travel preferences🏕️🌴
+      </h2>
       <p className="mt-3 text-gray-500 text-xl">
         Just provide some basic information, and our trip planner will generate
         a customized itinerary based on your preferences.
@@ -61,7 +110,7 @@ function CreateTrip() {
               place,
               onChange: (v) => {
                 setPlace(v);
-                handleInputChange('location',v);
+                handleInputChange("location", v);
               },
             }}
           />
@@ -71,8 +120,10 @@ function CreateTrip() {
           <h2 className="text-xl my-3 font-medium">
             How many days are you planning your trip
           </h2>
-          <Input placeholder={"Ex. 3"} type="number" 
-            onChange={(e)=>handleInputChange('noOfDays', e.target.value)}
+          <Input
+            placeholder={"Ex. 3"}
+            type="number"
+            onChange={(e) => handleInputChange("noOfDays", e.target.value)}
           />
         </div>
 
@@ -83,9 +134,10 @@ function CreateTrip() {
               <div
                 key={index}
                 className={`p-4 border rounded-lg cursor-pointer hover:shadow-lg
-                  ${formData?.budget==item.title && 'shadow-lg border-black'}`
-                }
-                onClick={()=>handleInputChange('budget',item.title)}
+                  ${
+                    formData?.budget == item.title && "shadow-lg border-black"
+                  }`}
+                onClick={() => handleInputChange("budget", item.title)}
               >
                 <h2 className="text-xl">{item.icon}</h2>
                 <h2 className="font-bold text-lg">{item.title}</h2>
@@ -96,14 +148,19 @@ function CreateTrip() {
         </div>
 
         <div>
-          <h2 className="text-xl my-3 font-medium">Who do you want to travel with on your next Adventure?</h2>
+          <h2 className="text-xl my-3 font-medium">
+            Who do you want to travel with on your next Adventure?
+          </h2>
           <div className="grid grid-cols-3 gap-5 mt-5">
             {SelectTravelesList.map((item, index) => (
               <div
                 key={index}
                 className={`p-4 border rounded-lg cursor-pointer hover:shadow-lg
-                  ${formData?.traveler==item.people && 'shadow-lg border-black'}`}
-                onClick={()=>handleInputChange('traveler',item.people)}
+                  ${
+                    formData?.traveler == item.people &&
+                    "shadow-lg border-black"
+                  }`}
+                onClick={() => handleInputChange("traveler", item.people)}
               >
                 <h2 className="text-xl">{item.icon}</h2>
                 <h2 className="font-bold text-lg">{item.title}</h2>
@@ -115,9 +172,33 @@ function CreateTrip() {
 
         <div className="my-50 justify-end flex">
           <Button onClick={onGenerateTrip}>Generate Trip</Button>
-          
         </div>
 
+        <Dialog open={openDialog}>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="sr-only">
+                Are you absolutely sure?
+              </DialogTitle>
+              <DialogDescription>
+                <img src="/logo.svg" />
+                <>
+                  <h2 className="font-bold text-lg mt-7">
+                    Sign In With Google
+                  </h2>
+                  <p>Sign in to the app with Google Authentication</p>
+                </>
+                <Button
+                  className="w-full mt-5 flex gap-2 items-center"
+                  onClick={login}
+                >
+                  <FcGoogle />
+                  Sign In With Google
+                </Button>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import CreateTrip from './create-trip/index.jsx'
-import Header from './components/custom/Header.jsx'
-import { Toaster } from 'sonner'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreateTrip from "./create-trip/index.jsx";
+import Header from "./components/custom/Header.jsx";
+import { Toaster } from "sonner";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element:<App/>
+    path: "/",
+    element: <App />,
   },
   {
-    path: '/create-trip',
-    element:<CreateTrip/>
-  }
-])
+    path: "/create-trip",
+    element: <CreateTrip />,
+  },
+]);
 
-createRoot(document.getElementById('module')).render(
+createRoot(document.getElementById("module")).render(
   <StrictMode>
-    <Header />
-    <Toaster />
-    <RouterProvider router={router}/> 
-  </StrictMode>,
-)
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+      <Header />
+      <Toaster />
+      <RouterProvider router={router} />
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
