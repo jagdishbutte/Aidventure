3fb2305fac58da32e27ed7244a0dc80dcc6a1672
jagdishbutte <jagdishbutte@gmail.com>
2024-11-14 00:22:37 +0530

    infoSection to InfoSection

--- src/view-trip/[tripId]/index.jsx
+++ src/view-trip/[tripId]/index.jsx
@@ -3,10 +3,10 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { toast } from 'sonner';
 import { db } from '../../service/firebaseConfig';
-import InfoSection from '../components/infoSection';
 import Hotels from '../components/Hotels';
 import PlacesToVisit from '../components/PlacesToVisit';
 import Footer from '../components/Footer';
+import InfoSection from '../components/InfoSection';
 
 function Viewtrip() {
     const {tripId} = useParams();
